9423dfc213929d9ff405058dde6a18ea6a457628
Rui Nabeshima <ruinabeshima0321@gmail.com>
2026-09-22 22:12:46 +0900

    feat: add logic to parse command for simple strings

--- main.go
+++ main.go
@@ -18,6 +18,19 @@ func main() {
 	switch testBytes[0] {
 	case simpleString:
 		fmt.Println("Simple string")
+
+		if len(testBytes) == 1 {
+			fmt.Println("No command")
+			return
+		}
+
+		end := 1
+		for end < len(testBytes) && !is_CRLF(testBytes, end) {
+			end += 1
+		}
+		command := string(testBytes[1:end])
+		fmt.Println("Command: ", command)
+
 	case simpleError:
 		fmt.Println("Simple error")
 	case integer:
@@ -30,18 +43,17 @@ func main() {
 		fmt.Println("Unknown / invalid command")
 	}
 
-	fmt.Println("Your command is:", testBytes)
 }
 
-func is_CRLF(str string, pointer int) bool {
+func is_CRLF(byteArray []byte, pointer int) bool {
 
 	// Pointer out of bounds
-	if pointer+1 >= len(str) || pointer < 0 {
+	if pointer+1 >= len(byteArray) || pointer < 0 {
 		return false
 	}
 
 	// Check if there is a Carriage Return Line Feed (\r\n)
-	if str[pointer] == '\r' && str[pointer+1] == '\n' {
+	if byteArray[pointer] == '\r' && byteArray[pointer+1] == '\n' {
 		return true
 	}
 
