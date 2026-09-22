1a0c1d40e709830a8fa4b16b65a9445361997ce1
Rui Nabeshima <ruinabeshima0321@gmail.com>
2026-09-22 21:57:14 +0900

    fix: format and convert to string from byte array

--- main.go
+++ main.go
@@ -1,62 +1,61 @@
-package main 
+package main
 
-import (
-	"fmt"
-	"bufio"
-	"os"
-)
+import "fmt"
 
 // Data types correspond to symbol of first byte
 const (
 	simpleString = '+'
-	simpleError = '-'
-	integer = ':'
-	bulkString = '$'
-	array = '*'
+	simpleError  = '-'
+	integer      = ':'
+	bulkString   = '$'
+	array        = '*'
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
+	testInput := "+OK\r\n"
+	testBytes := []byte(testInput)
 
-	fmt.Println("Enter RESP command: ")
-	if scanner.Scan() {
-		input := scanner.Text() 
-
-		if len(input) == 0 {
-			fmt.Println("Please provide an input")
-			return
-		}
-
-		switch input[0] {
-			case simpleString:
-				fmt.Println("Simple string")
-			case simpleError:
-				fmt.Println("Simple error")
-			case integer: 
-				fmt.Println("Integer")
-			case bulkString:
-				fmt.Println("Bulk string")
-			case array:
-				fmt.Println("Array")
-			default:
-				fmt.Println("Unknown / invalid command")
-		}
-
-		fmt.Println("Your command is:", input)
+	// Convert byte stream to string
+	input := ""
+	for i := 0; i < len(testBytes); i++ {
+		char := string(testBytes[i])
+		input += char
 	}
+
+	if len(input) == 0 {
+		fmt.Println("Please provide an input")
+		return
+	}
+
+	switch input[0] {
+	case simpleString:
+		fmt.Println("Simple string")
+	case simpleError:
+		fmt.Println("Simple error")
+	case integer:
+		fmt.Println("Integer")
+	case bulkString:
+		fmt.Println("Bulk string")
+	case array:
+		fmt.Println("Array")
+	default:
+		fmt.Println("Unknown / invalid command")
+	}
+
+	fmt.Println("Your command is:", input)
 }
 
 func is_CRLF(str string, pointer int) bool {
 
 	// Pointer out of bounds
-	if pointer + 1 >= len(str) || pointer < 0 {
+	if pointer+1 >= len(str) || pointer < 0 {
 		return false
 	}
 
 	// Check if there is a Carriage Return Line Feed (\r\n)
-	if str[pointer] == '\r' && str[pointer + 1] == '\n' {
-		return true 
+	if str[pointer] == '\r' && str[pointer+1] == '\n' {
+		return true
 	}
 
-	return false 
+	return false
 }
