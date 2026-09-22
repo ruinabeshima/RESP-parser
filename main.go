f91baa47b129da6c860ffda71cd92c146abd95cb
Rui Nabeshima <ruinabeshima0321@gmail.com>
2026-09-22 20:53:26 +0900

    feat: create function to check for CRLF in string

--- main.go
+++ main.go
@@ -24,3 +24,18 @@ func main() {
 		fmt.Println("Your command is:", input)
 	}
 }
+
+func is_CRLF(str string, pointer int) bool {
+
+	// Pointer out of bounds
+	if pointer + 1 >= len(str) || pointer < 0 {
+		return false
+	}
+
+	// Check if there is a Carriage Return Line Feed (\r\n)
+	if str[pointer] == '\r' && str[pointer + 1] == '\n' {
+		return true 
+	}
+
+	return false 
+}
