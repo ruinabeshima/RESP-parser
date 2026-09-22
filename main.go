df396e60c9d8ba76296488d5151cf350f8b992de
Rui Nabeshima <ruinabeshima0321@gmail.com>
2026-09-22 10:26:28 +0900

    feat: add first byte constants for data types

--- main.go
+++ main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+const (
+	simpleString = "+"
+	simpleError = "-"
+	integer = ":"
+	bulkString = "$"
+	array = "*"
+)
+
 func main() {
 	fmt.Println("Hello World!")
 }
